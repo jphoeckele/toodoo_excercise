0a0a3ae48ef0ead0429dd68e2c92703f1ebffbcb
jphoeckele <jphoeckele@gmail.com>
2015-10-29 17:24:33 -0400

    initial commit

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,11 @@
+$(document).on("ready page:load", ready);
+function ready() {
+	$('#new_task').on('ajax:success', newTask)
+	function newTask(event, data) {
+		$('#tasks').append(data);
+		$('#task_name').val('');
+		console.log(data);
+
+	};
+
+}
